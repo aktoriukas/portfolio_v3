4d7e9a0040cf1fb3dae7f7f2f4dec57bc3f17726
aktoriukas <gediminas.strumila@gmail.com>
2021-02-19 14:26:18 +0000

    new skills design

--- src/elements/Skills.js
+++ src/elements/Skills.js
@@ -6,20 +6,26 @@ import Skill from './Skill';
 export default class Skills extends Component {
     render() {
         return (
-            <ul className='skills'>
+            <div className='skills'>
                 {Database.skills.map((skill) => (
-                    <li
+                    <div
                         key={skill.id}
                         className='skill'
                     >
-                    <h3>{skill.title}</h3>
-                        <Skill 
-                            skill={skill}
-                            key={skill.id}
-                        />
-                    </li>
+                        <div className='skill-inner'>
+                            <div className='front'>
+                                <h3>{skill.title}</h3>
+                            </div>
+                            <div className='back'>
+                                <Skill
+                                    skill={skill}
+                                    key={skill.id}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 ))}
-            </ul>
+            </div>
         )
     }
 }
